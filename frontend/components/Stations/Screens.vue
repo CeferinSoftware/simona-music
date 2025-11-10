a960d296f622367babfd6e7ec0de42e5d86d3372
CeferinSoftware <newshellweb@gmail.com>
2025-11-11 00:52:53 +0100

    fix: Screens.vue usar ScreensManager sin Vuelidate

--- frontend/components/Stations/Screens.vue
+++ frontend/components/Stations/Screens.vue
@@ -3,7 +3,7 @@
 </template>
 
 <script setup lang="ts">
-import ScreensManager from '~/components/Stations/Screens/ScreensManager.vue';
+import ScreensManager from '~/components/Stations/ScreensManager.vue';
 import {getStationApiUrl} from '~/router';
 
 const listUrl = getStationApiUrl('/screens');
